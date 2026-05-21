424b637e0f9885b1bcb2c2c41629dd76ceb5817c
rvkeerthi24 <rvkeerthi99@gmail.com>
2026-05-21 19:10:53 +0530

    structure changes

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black text-white text-5xl font-bold">
+      Restaurant POS
+    </div>
+  )
+}
+
+export default App
